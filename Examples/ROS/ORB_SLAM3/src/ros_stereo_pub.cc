599fbe580bdec504ad05b2a0bbb0f3467ef9a731
MoDongbao <72547107+MoDongbao@users.noreply.github.com>
2023-04-23 09:20:54 +0800

    Add files via upload

---
+++ Examples/ROS/ORB_SLAM3/src/ros_stereo_pub.cc
@@ -0,0 +1,372 @@
+/**
+* This file is part of ORB-SLAM3
+*
+* Copyright (C) 2017-2020 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+*
+* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <unistd.h>
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include "sensor_msgs/PointCloud2.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/PoseArray.h"
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include<opencv2/core/core.hpp>
+
+#include"../../../include/System.h"
+
+#include "MapPoint.h"
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <Converter.h>
+
+using namespace std;
+
+int pub_count = 0;
+int all_pts_pub_gap = 0;
+bool pub_all_pts = false;
+
+void publish(ORB_SLAM3::System &SLAM, ros::Publisher &pub_pts_and_pose,
+	ros::Publisher &pub_all_kf_and_pts, int frame_id);
+
+class ImageGrabber
+{
+public:
+    // ImageGrabber(ORB_SLAM3::System* pSLAM):mpSLAM(pSLAM){}
+    ImageGrabber(ORB_SLAM3::System &_SLAM, ros::Publisher &_pub_pts_and_pose,
+		ros::Publisher &_pub_all_kf_and_pts) :
+		SLAM(_SLAM), pub_pts_and_pose(_pub_pts_and_pose),
+		pub_all_kf_and_pts(_pub_all_kf_and_pts), frame_id(0){}
+
+    void GrabStereo(const sensor_msgs::ImageConstPtr& msgLeft,const sensor_msgs::ImageConstPtr& msgRight);
+
+    // ORB_SLAM3::System* mpSLAM;
+    ORB_SLAM3::System &SLAM;
+	ros::Publisher &pub_pts_and_pose;
+	ros::Publisher &pub_all_kf_and_pts;
+	int frame_id;
+    bool do_rectify;
+    cv::Mat M1l,M2l,M1r,M2r;
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "Stereo");
+    ros::start();
+
+    if(argc != 4)
+    {
+        cerr << endl << "Usage: rosrun ORB_SLAM3 Stereo path_to_vocabulary path_to_settings do_rectify" << endl;
+        ros::shutdown();
+        return 1;
+    }    
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::STEREO,true);
+
+    ros::NodeHandle nodeHandler;
+	ros::Publisher pub_pts_and_pose = nodeHandler.advertise<geometry_msgs::PoseArray>("pts_and_pose", 1000);
+	ros::Publisher pub_all_kf_and_pts = nodeHandler.advertise<geometry_msgs::PoseArray>("all_kf_and_pts", 1000);
+
+    // ImageGrabber igb(&SLAM);
+    ImageGrabber igb(SLAM, pub_pts_and_pose, pub_all_kf_and_pts);
+
+    stringstream ss(argv[3]);
+	ss >> boolalpha >> igb.do_rectify;
+
+    if(igb.do_rectify)
+    {      
+        // Load settings related to stereo calibration
+        cv::FileStorage fsSettings(argv[2], cv::FileStorage::READ);
+        if(!fsSettings.isOpened())
+        {
+            cerr << "ERROR: Wrong path to settings" << endl;
+            return -1;
+        }
+
+        cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
+        fsSettings["LEFT.K"] >> K_l;
+        fsSettings["RIGHT.K"] >> K_r;
+
+        fsSettings["LEFT.P"] >> P_l;
+        fsSettings["RIGHT.P"] >> P_r;
+
+        fsSettings["LEFT.R"] >> R_l;
+        fsSettings["RIGHT.R"] >> R_r;
+
+        fsSettings["LEFT.D"] >> D_l;
+        fsSettings["RIGHT.D"] >> D_r;
+
+        int rows_l = fsSettings["LEFT.height"];
+        int cols_l = fsSettings["LEFT.width"];
+        int rows_r = fsSettings["RIGHT.height"];
+        int cols_r = fsSettings["RIGHT.width"];
+
+        if(K_l.empty() || K_r.empty() || P_l.empty() || P_r.empty() || R_l.empty() || R_r.empty() || D_l.empty() || D_r.empty() ||
+                rows_l==0 || rows_r==0 || cols_l==0 || cols_r==0)
+        {
+            cerr << "ERROR: Calibration parameters to rectify stereo are missing!" << endl;
+            return -1;
+        }
+
+        cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l.rowRange(0,3).colRange(0,3),cv::Size(cols_l,rows_l),CV_32F,igb.M1l,igb.M2l);
+        cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,igb.M1r,igb.M2r);
+    }
+
+    ros::NodeHandle nh;
+    message_filters::Subscriber<sensor_msgs::Image> left_sub(nh, "/camera/left/image_raw", 1);
+    message_filters::Subscriber<sensor_msgs::Image> right_sub(nh, "/camera/right/image_raw", 1);
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
+    message_filters::Synchronizer<sync_pol> sync(sync_pol(10), left_sub,right_sub);
+    sync.registerCallback(boost::bind(&ImageGrabber::GrabStereo,&igb,_1,_2));
+
+    ros::spin();
+
+
+    mkdir("results", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	SLAM.getMap()->GetCurrentMap()->Save("results//map_pts_out.obj");
+	SLAM.getMap()->GetCurrentMap()->SaveWithTimestamps("results//map_pts_and_keyframes.txt");
+    // Save camera trajectory
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory_TUM_Format.txt");
+    SLAM.SaveTrajectoryTUM("FrameTrajectory_TUM_Format.txt");
+    SLAM.SaveTrajectoryKITTI("FrameTrajectory_KITTI_Format.txt");
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    ros::shutdown();
+
+    return 0;
+}
+
+void ImageGrabber::GrabStereo(const sensor_msgs::ImageConstPtr& msgLeft,const sensor_msgs::ImageConstPtr& msgRight)
+{
+    // Copy the ros image message to cv::Mat.
+    cv_bridge::CvImageConstPtr cv_ptrLeft;
+    try
+    {
+        cv_ptrLeft = cv_bridge::toCvShare(msgLeft);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    cv_bridge::CvImageConstPtr cv_ptrRight;
+    try
+    {
+        cv_ptrRight = cv_bridge::toCvShare(msgRight);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    if(do_rectify)
+    {
+        cv::Mat imLeft, imRight;
+        cv::remap(cv_ptrLeft->image,imLeft,M1l,M2l,cv::INTER_LINEAR);
+        cv::remap(cv_ptrRight->image,imRight,M1r,M2r,cv::INTER_LINEAR);
+        SLAM.TrackStereo(imLeft,imRight,cv_ptrLeft->header.stamp.toSec());
+        publish(SLAM, pub_pts_and_pose, pub_all_kf_and_pts, frame_id);
+        ++frame_id;
+    }
+    else
+    {
+        SLAM.TrackStereo(cv_ptrLeft->image,cv_ptrRight->image,cv_ptrLeft->header.stamp.toSec());
+        publish(SLAM, pub_pts_and_pose, pub_all_kf_and_pts, frame_id);
+        ++frame_id;
+    }
+}
+
+void publish(ORB_SLAM3::System &SLAM, ros::Publisher &pub_pts_and_pose,
+	ros::Publisher &pub_all_kf_and_pts, int frame_id) {
+	if (all_pts_pub_gap>0 && pub_count >= all_pts_pub_gap) {
+		pub_all_pts = true;
+		pub_count = 0;
+	}
+	if (pub_all_pts || SLAM.getLoopClosing()->loop_detected || SLAM.getTracker()->loop_detected) {
+		pub_all_pts = SLAM.getTracker()->loop_detected = SLAM.getLoopClosing()->loop_detected = false;
+		geometry_msgs::PoseArray kf_pt_array;
+		vector<ORB_SLAM3::KeyFrame*> key_frames = SLAM.getMap()->GetCurrentMap()->GetAllKeyFrames();
+		//! placeholder for number of keyframes
+		kf_pt_array.poses.push_back(geometry_msgs::Pose());
+		sort(key_frames.begin(), key_frames.end(), ORB_SLAM3::KeyFrame::lId);
+		unsigned int n_kf = 0;
+		for (auto key_frame : key_frames) {
+			// pKF->SetPose(pKF->GetPose()*Two);
+
+			if (key_frame->isBad())
+				continue;
+
+			cv::Mat R = key_frame->GetRotation().t();
+			vector<float> q = ORB_SLAM3::Converter::toQuaternion(R);
+			cv::Mat twc = key_frame->GetCameraCenter();
+			geometry_msgs::Pose kf_pose;
+
+			kf_pose.position.x = twc.at<float>(0);
+			kf_pose.position.y = twc.at<float>(1);
+			kf_pose.position.z = twc.at<float>(2);
+			kf_pose.orientation.x = q[0];
+			kf_pose.orientation.y = q[1];
+			kf_pose.orientation.z = q[2];
+			kf_pose.orientation.w = q[3];
+			kf_pt_array.poses.push_back(kf_pose);
+
+			unsigned int n_pts_id = kf_pt_array.poses.size();
+			//! placeholder for number of points
+			kf_pt_array.poses.push_back(geometry_msgs::Pose());
+			std::set<ORB_SLAM3::MapPoint*> map_points = key_frame->GetMapPoints();
+			unsigned int n_pts = 0;
+			for (auto map_pt : map_points) {
+				if (!map_pt || map_pt->isBad()) {
+					//printf("Point %d is bad\n", pt_id);
+					continue;
+				}
+				cv::Mat pt_pose = map_pt->GetWorldPos();
+				if (pt_pose.empty()) {
+					//printf("World position for point %d is empty\n", pt_id);
+					continue;
+				}
+				geometry_msgs::Pose curr_pt;
+				//printf("wp size: %d, %d\n", wp.rows, wp.cols);
+				//pcl_cloud->push_back(pcl::PointXYZ(wp.at<float>(0), wp.at<float>(1), wp.at<float>(2)));
+				curr_pt.position.x = pt_pose.at<float>(0);
+				curr_pt.position.y = pt_pose.at<float>(1);
+				curr_pt.position.z = pt_pose.at<float>(2);
+				kf_pt_array.poses.push_back(curr_pt);
+				++n_pts;
+			}
+			geometry_msgs::Pose n_pts_msg;
+			n_pts_msg.position.x = n_pts_msg.position.y = n_pts_msg.position.z = n_pts;
+			kf_pt_array.poses[n_pts_id] = n_pts_msg;
+			++n_kf;
+		}
+		geometry_msgs::Pose n_kf_msg;
+		n_kf_msg.position.x = n_kf_msg.position.y = n_kf_msg.position.z = n_kf;
+		kf_pt_array.poses[0] = n_kf_msg;
+		kf_pt_array.header.frame_id = "1";
+		kf_pt_array.header.seq = frame_id + 1;
+		printf("Publishing data for %u keyfranmes\n", n_kf);
+		pub_all_kf_and_pts.publish(kf_pt_array);
+	}
+	else if (SLAM.getTracker()->mCurrentFrame.is_keyframe) {
+		++pub_count;
+		SLAM.getTracker()->mCurrentFrame.is_keyframe = false;
+		ORB_SLAM3::KeyFrame* pKF = SLAM.getTracker()->mCurrentFrame.mpReferenceKF;
+
+		cv::Mat Trw = cv::Mat::eye(4, 4, CV_32F);
+
+		// If the reference keyframe was culled, traverse the spanning tree to get a suitable keyframe.
+		//while (pKF->isBad())
+		//{
+		//	Trw = Trw*pKF->mTcp;
+		//	pKF = pKF->GetParent();
+		//}
+
+		vector<ORB_SLAM3::KeyFrame*> vpKFs = SLAM.getMap()->GetCurrentMap()->GetAllKeyFrames();
+		sort(vpKFs.begin(), vpKFs.end(), ORB_SLAM3::KeyFrame::lId);
+
+		// Transform all keyframes so that the first keyframe is at the origin.
+		// After a loop closure the first keyframe might not be at the origin.
+		cv::Mat Two = vpKFs[0]->GetPoseInverse();
+
+		Trw = Trw*pKF->GetPose()*Two;
+		cv::Mat lit = SLAM.getTracker()->mlRelativeFramePoses.back();
+		cv::Mat Tcw = lit*Trw;
+		cv::Mat Rwc = Tcw.rowRange(0, 3).colRange(0, 3).t();
+		cv::Mat twc = -Rwc*Tcw.rowRange(0, 3).col(3);
+
+		vector<float> q = ORB_SLAM3::Converter::toQuaternion(Rwc);
+		//geometry_msgs::Pose camera_pose;
+		//std::vector<ORB_SLAM3::MapPoint*> map_points = SLAM.getMap()->GetCurrentMap()->GetAllMapPoints();
+		std::vector<ORB_SLAM3::MapPoint*> map_points = SLAM.GetTrackedMapPoints();
+		int n_map_pts = map_points.size();
+
+		//printf("n_map_pts: %d\n", n_map_pts);
+
+		//pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+		geometry_msgs::PoseArray pt_array;
+		//pt_array.poses.resize(n_map_pts + 1);
+
+		geometry_msgs::Pose camera_pose;
+
+		camera_pose.position.x = twc.at<float>(0);
+		camera_pose.position.y = twc.at<float>(1);
+		camera_pose.position.z = twc.at<float>(2);
+
+		camera_pose.orientation.x = q[0];
+		camera_pose.orientation.y = q[1];
+		camera_pose.orientation.z = q[2];
+		camera_pose.orientation.w = q[3];
+
+		pt_array.poses.push_back(camera_pose);
+
+		//printf("Done getting camera pose\n");
+
+		for (int pt_id = 1; pt_id <= n_map_pts; ++pt_id){
+
+			if (!map_points[pt_id - 1] || map_points[pt_id - 1]->isBad()) {
+				//printf("Point %d is bad\n", pt_id);
+				continue;
+			}
+			cv::Mat wp = map_points[pt_id - 1]->GetWorldPos();
+
+			if (wp.empty()) {
+				//printf("World position for point %d is empty\n", pt_id);
+				continue;
+			}
+			geometry_msgs::Pose curr_pt;
+			//printf("wp size: %d, %d\n", wp.rows, wp.cols);
+			//pcl_cloud->push_back(pcl::PointXYZ(wp.at<float>(0), wp.at<float>(1), wp.at<float>(2)));
+			curr_pt.position.x = wp.at<float>(0);
+			curr_pt.position.y = wp.at<float>(1);
+			curr_pt.position.z = wp.at<float>(2);
+			pt_array.poses.push_back(curr_pt);
+			//printf("Done getting map point %d\n", pt_id);
+		}
+		//sensor_msgs::PointCloud2 ros_cloud;
+		//pcl::toROSMsg(*pcl_cloud, ros_cloud);
+		//ros_cloud.header.frame_id = "1";
+		//ros_cloud.header.seq = ni;
+
+		//printf("valid map pts: %lu\n", pt_array.poses.size()-1);
+
+		//printf("ros_cloud size: %d x %d\n", ros_cloud.height, ros_cloud.width);
+		//pub_cloud.publish(ros_cloud);
+		pt_array.header.frame_id = "1";
+		pt_array.header.seq = frame_id + 1;
+		pub_pts_and_pose.publish(pt_array);
+		//pub_kf.publish(camera_pose);
+	}
+}
+
+
